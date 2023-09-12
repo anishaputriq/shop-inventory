227dc677b875c57b3f18fec6fa22320bf598c5fa
Anisha Putri Qonitah <nishaputriq@gmail.com>
2023-09-12 23:38:33 +0700

    feat: admin item dll

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def show_main(request):
+    items = Item.objects.all()  # Mengambil semua objek Item dari basis data
     context = {
-        'name': 'Anisha Putri Qonitah',
-        'class': 'PBP D'
+        'items': items,
     }
 
     return render(request, "main.html", context)
