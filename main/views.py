f4327b271efe5c14c0979b56cec5b6bd8abaf480
Anisha Putri Qonitah <nishaputriq@gmail.com>
2023-09-16 23:54:39 +0700

    feat: formspy

--- main/views.py
+++ main/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from main.forms import ItemForm
+from django.urls import reverse
+from main.models import Item
 
 # Create your views here.
 def show_main(request):
-    items = [
-        {
-            'name': 'Beef Noodle :)',
-            'amount': 8,
-            'description': 'Mie rasa daging',
-        },
-        {
-            'name': 'Chicken Rice >_<',
-            'amount': 12,
-            'description': 'Nasi dengan ayam',
-        },
-        {
-            'name': 'Chitatoo ;)',
-            'amount': 26,
-            'description': 'Crispy potato chips',
-        },
-    ]
+    products = Item.objects.all()
 
     context = {
-        'items': items,
-        'name' : 'Anisha Putri Qonitah',
-        'class' : 'PBP D'
+        'name': 'Anisha Putri Qonitah', # Nama kamu
+        'class': 'PBP D', # Kelas PBP kamu
+        'products': products
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
--- shop_inventory/urls.py
+++ shop_inventory/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
 ]
 
